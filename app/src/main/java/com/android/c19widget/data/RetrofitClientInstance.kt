bc35e81887f49744aed89b3b44f5fe6bd6cf41dd
Vague <vaguemail369@gmail.com>
2020-05-08 23:04:30 +0100

    Create the retrofit client instance Add Gson converter library

---
+++ app/src/main/java/com/android/c19widget/data/RetrofitClientInstance.kt
@@ -0,0 +1,23 @@
+package com.android.c19widget.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClientInstance {
+
+    companion object {
+        private var retrofit: Retrofit? = null
+        private val BASE_URL = "https://covid9ja.herokuapp.com"
+
+        fun getRetrofitInstance(): Retrofit {
+            retrofit ?: {
+                retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }()
+
+            return retrofit!!
+        }
+    }
+}
