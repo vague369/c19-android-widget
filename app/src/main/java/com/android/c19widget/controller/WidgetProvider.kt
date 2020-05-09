1a564c9d30fb35b313f177d048030879a5ed8df1
Vague <vaguemail369@gmail.com>
2020-05-09 13:12:07 +0100

    Create a worker util

--- app/src/main/java/com/android/c19widget/controller/WidgetProvider.kt
+++ app/src/main/java/com/android/c19widget/controller/WidgetProvider.kt
@@ -3,6 +3,7 @@ package com.android.c19widget.controller
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import androidx.work.ListenableWorker
 import com.android.c19widget.model.AppRepository
 import com.android.c19widget.model.Failure
 import com.android.c19widget.model.Repository
@@ -23,21 +24,26 @@ class WidgetProvider : AppWidgetProvider() {
     }
 
     @Suppress("MemberVisibilityCanBePrivate")
-    fun doUpdate(context: Context?, repository: Repository, view: View) {
-        context ?: return
+    fun doUpdate(context: Context?, repository: Repository, view: View): ListenableWorker.Result {
+        context ?: return ListenableWorker.Result.failure()
+        var hasFailed = true
         AppLogger.log("WidgetProvider trying to do update")
         repository.getData {
-            when (it) {
+            hasFailed = when (it) {
                 is Success -> {
                     AppLogger.log("Data came in alright")
                     val noc = it.data.first
                     val nod = it.data.second
                     if (noc.isNotEmpty() && nod.isNotEmpty()) view.update(context, noc, nod)
+                    false
                 }
                 is Failure -> {
                     AppLogger.cry(it.err)
+                    true
                 }
             }
         }
+
+        return if (hasFailed) ListenableWorker.Result.failure() else ListenableWorker.Result.success()
     }
 }
---
+++ app/src/main/java/com/android/c19widget/utils/Worker.kt
@@ -0,0 +1,15 @@
+package com.android.c19widget.utils
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.android.c19widget.controller.WidgetProvider
+import com.android.c19widget.model.AppRepository
+import com.android.c19widget.view.WidgetView
+
+class Worker(private val context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters) {
+    override fun doWork(): Result {
+        return WidgetProvider().doUpdate(context, AppRepository(), WidgetView())
+    }
+}
