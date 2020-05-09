e56db29a6c83fd70f88ccef990274c8d8fba5f38
Vague <vaguemail369@gmail.com>
2020-05-09 11:07:00 +0100

    Create a view for the widget

---
+++ app/src/main/java/com/android/c19widget/view/WidgetView.kt
@@ -0,0 +1,8 @@
+package com.android.c19widget.view
+
+class WidgetView : View {
+
+    override fun updateView(noc: String, nod: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
