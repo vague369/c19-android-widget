edd51272f5f8312a2fa0c63a18a028ff1845bcec
Vague <vaguemail369@gmail.com>
2020-05-09 12:39:11 +0100

    Launch main activity when user clicks on widget

--- app/src/main/java/com/android/c19widget/view/WidgetView.kt
+++ app/src/main/java/com/android/c19widget/view/WidgetView.kt
@@ -1,10 +1,13 @@
 package com.android.c19widget.view
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import com.android.c19widget.R
+import com.android.c19widget.controller.MainActivity
 import com.android.c19widget.controller.WidgetProvider
 import com.android.c19widget.utils.AppLogger
 
@@ -15,6 +18,12 @@ class WidgetView : View {
         val updatedViews = RemoteViews(context.packageName, R.layout.layout_appwidget).apply {
             setTextViewText(R.id.WidgetNoc, noc)
             setTextViewText(R.id.WidgetNod, "$nod deaths")
+
+            val intent = Intent(context, MainActivity::class.java)
+            setOnClickPendingIntent(
+                R.id.Widget,
+                PendingIntent.getActivity(context, 0, intent, 0)
+            )
         }
 
         val thisWidget = ComponentName(context, WidgetProvider::class.java)
