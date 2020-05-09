911877c1e8f5e5e365161f866a265373be1c75b4
Vague <vaguemail369@gmail.com>
2020-05-09 11:18:15 +0100

    Implement widget view update

--- app/src/main/java/com/android/c19widget/view/WidgetView.kt
+++ app/src/main/java/com/android/c19widget/view/WidgetView.kt
@@ -1,10 +1,25 @@
 package com.android.c19widget.view
 
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
 import android.content.Context
+import android.widget.RemoteViews
+import com.android.c19widget.R
+import com.android.c19widget.controller.WidgetProvider
+import com.android.c19widget.utils.AppLogger
 
 class WidgetView : View {
 
     override fun update(context: Context, noc: String, nod: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        AppLogger.log("Updating Widget")
+        val updatedViews = RemoteViews(context.packageName, R.layout.layout_appwidget).apply {
+            setTextViewText(R.id.WidgetNoc, noc)
+            setTextViewText(R.id.WidgetNod, "$nod deaths")
+        }
+
+        val thisWidget = ComponentName(context, WidgetProvider::class.java)
+        AppWidgetManager.getInstance(context).apply {
+            updateAppWidget(thisWidget, updatedViews)
+        }
     }
 }
