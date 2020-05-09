7b6ac43a3093133e5a5777fb97618fb164edd63a
Vague <vaguemail369@gmail.com>
2020-05-09 10:01:01 +0100

    Create class to retrieve data from api

---
+++ app/src/main/java/com/android/c19widget/data/DataGetter.kt
@@ -0,0 +1,6 @@
+package com.android.c19widget.data
+
+interface DataGetter {
+
+    fun getData(): List<Response>?
+}
---
+++ app/src/main/java/com/android/c19widget/data/DataGetterImpl.kt
@@ -0,0 +1,8 @@
+package com.android.c19widget.data
+
+class DataGetterImpl : DataGetter {
+
+    override fun getData(): List<Response>? {
+        return null
+    }
+}
