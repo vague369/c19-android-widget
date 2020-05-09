d3e887a7f329e7b170ed3e1a160b6c65d630390b
Vague <vaguemail369@gmail.com>
2020-05-09 11:27:11 +0100

    Create skeleton for getting updates

--- app/src/main/java/com/android/c19widget/controller/MainActivity.kt
+++ app/src/main/java/com/android/c19widget/controller/MainActivity.kt
@@ -3,11 +3,18 @@ package com.android.c19widget.controller
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.android.c19widget.R
+import com.android.c19widget.model.AppRepository
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doUpdate()
+    }
+
+    private fun doUpdate() {
+        WidgetProvider().doUpdate(AppRepository())
     }
 }
--- app/src/main/java/com/android/c19widget/controller/WidgetProvider.kt
+++ app/src/main/java/com/android/c19widget/controller/WidgetProvider.kt
@@ -1,7 +1,24 @@
 package com.android.c19widget.controller
 
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import com.android.c19widget.model.AppRepository
+import com.android.c19widget.model.Repository
 
 
 class WidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        doUpdate(AppRepository())
+    }
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    fun doUpdate(repository: Repository) {
+
+    }
 }
